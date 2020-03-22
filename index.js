79eb444bc0b070e1a02888ad238a55adae41c6e4
Nublo <varyvonchyk.anatol@gmail.com>
2020-03-22 22:30:51 +0000

    Refactoring

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const KEY_CACHE = "amount_of_cases"
 const CACHE_TTL = 10000
+const BOT_ID = "1114913919:AAHcUunychWYJbL9JSknBxyAbt7NXxlnGKk"
+const FAILED_API_MESSAGE = "Failed to get info. You can [check manually](https://www.worldometers.info/coronavirus/)"
 
 var express = require('express')
 var app = express()
@@ -9,14 +11,13 @@ const axios = require('axios')
 const NodeCache = require('node-cache');
 const cache = new NodeCache();
 
-app.use(bodyParser.json()) // for parsing application/json
+app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
     extended: true
   })
-) // for parsing application/x-www-form-urlencoded
+)
 
-//This is the route the API will call
 app.post('/new-message', function(req, res) {
   const { message } = req.body
   if (!message) {
@@ -28,7 +29,7 @@ app.post('/new-message', function(req, res) {
     requestCountInfo(message, res)
   } else {
     console.log("Cache hit")
-    postTGMessage(message, getAnswer(totalCases))
+    sendMessage(message, totalCases)
     res.sendStatus(200)
   }
 })
@@ -41,38 +42,34 @@ function requestCountInfo(message, res) {
      timeout: 5000
   }).then(response => {
     if (response.status === 200) {
-        const html = response.data
-        const $ = cheerio.load(html)
+        const $ = cheerio.load(response.data)
         var count = $("body div.container div.row div.col-md-8 div.content-inner div#maincounter-wrap[style='margin-top:15px'] span[style='color:#aaa']").text()
-        postTGMessage(message, getAnswer(count))
         cache.set(KEY_CACHE, count, CACHE_TTL)
+        console.log("Cache miss")
+        sendMessage(message, count)
         res.sendStatus(200)
     }
   }).catch(err => {
-    postTGMessage(message, "Failed to get info. You can [check manually](https://www.worldometers.info/coronavirus/)")
+    sendMessage(message, FAILED_API_MESSAGE)
     res.sendStatus(200)
   })
 }
 
-function postTGMessage(message, textToSend) {
+function sendMessage(message, cases) {
   axios
     .post(
-      'https://api.telegram.org/bot1114913919:AAHcUunychWYJbL9JSknBxyAbt7NXxlnGKk/sendMessage',
+      'https://api.telegram.org/bot' + BOT_ID +'/sendMessage',
       {
         chat_id: message.chat.id,
-        text: textToSend
+        text: "Total amount of infected - " + cases
       }
     )
     .then(response => {
-      console.log('Posted message - ' + textToSend)
+      console.log('Total cases - ' + cases)
     })
     .catch(err => {
       console.log('TG Error :', err)
     })
-}
-
-function getAnswer(count) {
-  return "Total amount of infected - " + count;
 }
 
 var port = process.env.PORT
