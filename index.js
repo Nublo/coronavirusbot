88123acae7052bdee86cf0a48dfdc1f3ab0dce3d
Nublo <varyvonchyk.anatol@gmail.com>
2020-03-23 20:14:14 +0000

    Fixed syntax error

--- index.js
+++ index.js
@@ -19,8 +19,10 @@ bot.onText(/\/help/, (msg) => {
   bot.sendMessage(
     msg.chat.id, 
     "This bot can provide you current number of people infected by COVID-19. To get this information just type '/status'. Source of data is https://www.worldometers.info/coronavirus/. Bot also cache information and update it once in 10 min, so don't expect update in less that 10 min",
-    "reply_markup": {
-      "keyboard": [["/status"]]
+    { 
+      "reply_markup": {
+        "keyboard": [["/status"]]
+      }
     }
   )
 });
