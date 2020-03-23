e07d54cf3be56880872634dd3e21f63f9c33334f
Nublo <varyvonchyk.anatol@gmail.com>
2020-03-23 19:39:35 +0000

    Updated bot with node-telegram-bot-api npm module

--- index.js
+++ index.js
@@ -2,36 +2,23 @@ const KEY_CACHE = "amount_of_cases"
 const CACHE_TTL = 10000
 const FAILED_API_MESSAGE = "Failed to get info. You can [check manually](https://www.worldometers.info/coronavirus/)"
 
-var express = require('express')
-var app = express()
-var bodyParser = require('body-parser')
 const cheerio = require('cheerio')
 const axios = require('axios')
 const NodeCache = require('node-cache');
 const cache = new NodeCache();
 
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true
-  })
-)
+const TelegramBot = require('node-telegram-bot-api'); 
+const bot = new TelegramBot(process.env.BOT_ID, {polling: true});
 
-app.post('/new-message', function(req, res) {
-  const { message } = req.body
-  if (!message) {
-    return res.sendStatus(200)
-  }
-
+bot.on('message', (msg) => { 
   var totalCases = cache.get(KEY_CACHE)
   if (totalCases == undefined) {
-    requestCountInfo(message, res)
+    requestCountInfo(message)
   } else {
     console.log("Cache hit")
     sendMessage(message, totalCases)
-    res.sendStatus(200)
   }
-})
+});
 
 function requestCountInfo(message, res) {
   var url = 'https://www.worldometers.info/coronavirus/'
@@ -44,34 +31,14 @@ function requestCountInfo(message, res) {
         const $ = cheerio.load(response.data)
         var count = $("body div.container div.row div.col-md-8 div.content-inner div#maincounter-wrap[style='margin-top:15px'] span[style='color:#aaa']").text()
         cache.set(KEY_CACHE, count, CACHE_TTL)
-        console.log("Cache miss")
         sendMessage(message, count)
-        res.sendStatus(200)
     }
   }).catch(err => {
     sendMessage(message, FAILED_API_MESSAGE)
-    res.sendStatus(200)
   })
 }
 
 function sendMessage(message, cases) {
-  axios
-    .post(
-      'https://api.telegram.org/bot' + process.env.BOT_ID +'/sendMessage',
-      {
-        chat_id: message.chat.id,
-        text: "Total amount of infected - " + cases
-      }
-    )
-    .then(response => {
-      console.log('Total cases - ' + cases)
-    })
-    .catch(err => {
-      console.log('TG Error :', err)
-    })
+  bot.sendMessage(msg.chat.id, "Total amount of infected - " + cases);
+  console.log('Total cases - ' + cases)
 }
-
-var port = process.env.PORT
-app.listen(port, function() {
-  console.log('Telegram app listening - ' + port);
-})
