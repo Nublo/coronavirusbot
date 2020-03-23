33d09ff376d73b8da1529163d80fba97ce221a74
Nublo <varyvonchyk.anatol@gmail.com>
2020-03-23 22:50:23 +0000

    Replaced html invocation with lambda

--- index.js
+++ index.js
@@ -25,14 +25,19 @@ bot.onText(/\/help/, (msg) => {
 bot.onText(/\/status/, (message) => {
   var totalCases = cache.get(KEY_CACHE)
   if (totalCases == undefined) {
-    requestCountInfo(message)
+    requestHtml(message, function (html) { 
+        const $ = cheerio.load(html)
+        var count = $("body div.container div.row div.col-md-8 div.content-inner div#maincounter-wrap[style='margin-top:15px'] span[style='color:#aaa']").text()
+        cache.set(KEY_CACHE, count, CACHE_TTL)
+        sendMessage(message, count)
+    })
   } else {
     console.log("Cache hit")
     sendMessage(message, totalCases)
   }
 })
 
-function requestCountInfo(message, res) {
+function requestHtml(message, callback) {
   var url = 'https://www.worldometers.info/coronavirus/'
   axios({
      method: 'get',
@@ -40,10 +45,7 @@ function requestCountInfo(message, res) {
      timeout: 5000
   }).then(response => {
     if (response.status === 200) {
-        const $ = cheerio.load(response.data)
-        var count = $("body div.container div.row div.col-md-8 div.content-inner div#maincounter-wrap[style='margin-top:15px'] span[style='color:#aaa']").text()
-        cache.set(KEY_CACHE, count, CACHE_TTL)
-        sendMessage(message, count)
+        callback(response.data)
     }
   }).catch(err => {
     sendMessage(message, FAILED_API_MESSAGE)
